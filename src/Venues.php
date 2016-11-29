86ccac72c3cf3fb66ef2fcf7b93e797f01a0fd8a
mikeskiddle <mikesmedley@skiddle.com>
2016-11-29 08:52:11 +0000

    Add the venues class in now

---
+++ src/Venues.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Class Venues
+ *
+ * This class deals with creating calls to the venue endpoint.
+ * Any methods that can help to call venue info specifically belong in here
+ *
+ * @package  SkiddleSDK
+ */
+
+namespace SkiddleSDK;
+
+class Venues extends SkiddleBase {
+
+	/**
+	 * @var The endpoint we will be sending requests to
+	 */
+	const ENDPOINT = '/venues/';
+
+	/**
+	 * Inherit constructor from SkiddleBase
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * Pass arguments in and execute call
+	 * @param bool  $asArray Whether or not to return results as an array
+	 * @return obj An object of stuff
+	 */
+	public function getListings($asArray = false) {
+		return $this->makeCall(self::ENDPOINT,$asArray);
+	}
+
+}
