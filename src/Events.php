a0d7387965e545d002ad4fce19a8171f6334d547
michaelsmedley <mike@michael-smedley.co.uk>
2017-09-05 13:33:08 +0100

    New method to query individual listings

--- src/Events.php
+++ src/Events.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class Events
+ * Class SkiddleEventAPI
  *
  * This class deals with creating calls to the event endpoint.
  * Any methods that can help to call event info specifically belong in here
@@ -14,9 +14,14 @@ class Events extends SkiddleBase
 {
 
     /**
-     * @var The endpoint we will be sending requests to
+     * @var string The endpoint we will be sending requests to
      */
     const ENDPOINT = '/events/search/';
+
+    /**
+     * @var string The endpoint for calling single events
+     */
+    const SINGLE_ENDPOINT = '/events/';
 
     /**
      * Inherit constructor from SkiddleBase
@@ -29,11 +34,21 @@ class Events extends SkiddleBase
     /**
      * Pass arguments in and execute call
      * @param bool $asArray Whether or not to return results as an array
-     * @return obj An object of stuff
+     * @return object An object of stuff
      */
     public function getListings($asArray = false)
     {
         return $this->makeCall(self::ENDPOINT, $asArray);
+    }
+
+    /**
+     * Pass a listing instance id to get details about an individual event
+     * @param bool $listingId The listing instance id
+     * @return object An object with results
+     */
+    public function getListing($listingId = false)
+    {
+        return $this->makeCall(self::SINGLE_ENDPOINT . $listingId);
     }
 
 }
